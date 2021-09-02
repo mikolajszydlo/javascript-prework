6cc927c94380f0ac085549e61e59768ad4c7d3c3
Mikolaj Szydlo <mikolaj.szydlo@gmail.com>
2021-09-02 16:41:11 +0200

    Add condition for computer move

--- js/functions.js
+++ js/functions.js
@@ -7,9 +7,3 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-
-let computerMove = 'kamień'
-let playerMove = 'papier'
-
-printMessage('Komputer: Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Player One: Zagrałem ' + playerMove + '! :-)');
